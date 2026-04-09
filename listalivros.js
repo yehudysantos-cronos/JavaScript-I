a2a8aa8b3a2968793141f5d37a975dde8558d77e
yehudysantos-cronos <yehudy.santos@escola.pr.gov.br>
2026-04-09 13:33:53 +0000

    Trabalho finalizdo

---
+++ listalivros.js
@@ -0,0 +1,28 @@
+const precosLivros = [
+    {
+        titulo: "Noite na Taverna",
+        preco: 75
+    },
+    {
+        titulo: "Português",
+        preco: 85
+    },
+    {
+        titulo: "Harry Potter",
+        preco: 90
+    },
+    {
+        titulo: "Matemática",
+        preco: 70
+    },
+    {
+        titulo: "Constituição Federal",
+        preco: 100
+    },
+    {
+        titulo: "Bíblia",
+        preco: 50
+    }
+];
+
+module.exports = precosLivros;
